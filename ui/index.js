d84a2f7a81b6b953721dcd30b328d5606ea7416f
aykae <abdrai19@gmail.com>
2023-12-25 11:33:45 +0400

    handles multiple blocks

--- ui/index.js
+++ ui/index.js
@@ -1,25 +1,40 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const getBtn = document.querySelector("#mine-btn");
+//     getBtn.addEventListener("click", async () => {
+//         prevHash = "0";
+//         hash = "";
+//         while (hash != prevHash) {
+//             prevHash = hash;
+
+//             try {
+//                 const response = await fetch("http://localhost:8080/mine",
+//                     {
+//                         method: "GET",
+//                     });
+//                 console.log("Request sent.");
+//                 const mineObj = await response.json();
+
+//                 document.querySelector("#blockchain-length").innerText = mineObj["blen"];
+//                 document.querySelector("#mine-hash").innerText = mineObj["hash"];
+//                 hash = mineObj["hash"];
+
+//             } catch (error) {
+//                 console.log("Error fetching hash");
+//             }
+//         }
+//     });
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-    const getBtn = document.querySelector("#mine-btn");
-    getBtn.addEventListener("click", async () => {
-        prevHash = "0";
-        hash = "";
-        while (hash != prevHash) {
-            prevHash = hash;
-
-            try {
-                const response = await fetch("http://localhost:8080/mine",
-                    {
-                        method: "GET",
-                    });
-                console.log("Request sent.");
-                const mineObj = await response.json();
-
-                document.querySelector("#blockchain-length").innerText = mineObj["blen"];
-                document.querySelector("#mine-hash").innerText = mineObj["hash"];
-                hash = mineObj["hash"];
-
-            } catch (error) {
-                console.log("Error fetching hash");
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            const divToDuplicate = document.querySelector(".block");
+            if (divToDuplicate) {
+                const duplicateDiv = divToDuplicate.cloneNode(true);
+                console.log("Duplicated Block.")
+                document.querySelector(".block-container").appendChild(duplicateDiv);
+            } else {
+                console.log("No element with id 'block' found.");
             }
         }
     });
